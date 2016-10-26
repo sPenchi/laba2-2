faaf68e15d72f942d964b5d33b15ea3e9ee6cdbf
sPenchi <mr.pronenko@bk.ru>
2016-10-26 21:21:37 +0300

    the new tab control

--- WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Laba = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.Laba.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Laba
+            // 
+            this.Laba.Controls.Add(this.tabPage1);
+            this.Laba.Controls.Add(this.tabPage2);
+            this.Laba.ImeMode = System.Windows.Forms.ImeMode.Off;
+            this.Laba.Location = new System.Drawing.Point(12, 12);
+            this.Laba.Name = "Laba";
+            this.Laba.SelectedIndex = 0;
+            this.Laba.Size = new System.Drawing.Size(888, 508);
+            this.Laba.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(880, 482);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Приветствие";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(880, 482);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Калькулятор";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1272, 606);
+            this.Controls.Add(this.Laba);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Laba.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl Laba;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
